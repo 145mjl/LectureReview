e62909484eb4019e4ce77fdd2852d67899d5f94f
Y <Y@DESKTOP-S5KEKR6>
2022-11-15 22:47:15 +0900

    22-11-15

--- L221115/src/Constructor1/Car.java
+++ L221115/src/Constructor1/Car.java
@@ -56,16 +56,3 @@ public class Car {
 //note
 //System.out.println("car1.company : "+car1.company);에서 F5를 누르면 
 //생성자가 아니라 println메서드 설명으로 넘어간다
-
-//expressions에 멤버변수(company,model,color,maxSpeed)를 추가한 뒤 debug를 해보기
-//예를 들어 line 45에서 Step into하면
-//line19 생성자를 실행 중에는 멤버변수의 값이 생기지만
-//종료되면 전부 해제되는것을 볼 수 있음
-
-//한편 car1.company 등은 남는데
-//지역변수가 아니어서 --> X
-//객체의 인스턴스 속에 저장됨 --> O
-
-//Car의 멤버변수는 그럼 
-//car1.company, car2.company, ... 인가? -->X
-//멤버변수는 Car라는 틀에 있는 멤버 중 변수를 의미함
