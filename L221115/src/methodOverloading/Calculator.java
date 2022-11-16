59e92d1f9fd13a9483acc54eea482f1fea758c69
145mjl <145mjl@gmail.com>
2022-11-16 21:26:03 +0900

    22-11-16 생성자 오버라이딩

--- L221115/src/methodOverloading/Calculator.java
+++ L221115/src/methodOverloading/Calculator.java
@@ -11,6 +11,13 @@ public class Calculator {
 		return result;
 	}
 	
+//	double plus(double y,double z) {
+//		double result=y*z;
+//		return result;
+//	}
+//	Duplicate method plus(double, double) in type Calculator
+//	메인메서드에서 plus(실수,실수)를 호출시 두 메서드중 어떤것을 호출했는지 알수없게 된다 --> 애초에 작성금지
+	
 	public static void main(String[] args) {
 		Calculator cal = new Calculator();
 		int res1=cal.plus(5,10);
